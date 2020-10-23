c9acb057e04aecd292748db79ee64fac3d4ab026
khyeborg <kbl274@nyu.edu>
2020-10-23 15:59:45 -0400

    first commit

--- sketch.js
+++ sketch.js
@@ -4,9 +4,9 @@ let ballArray = [];
 function setup() {
    createCanvas(500, 500);
  
-   for (let i = 0; i < 3; i++) {
-       let temp = new Ball(random(0, 500), 250, 0, 255, 0, random(2, 5));
-       ballArray.push(temp);
+   for (let i = 0; i < 100; i++) {
+      let temp = new Ball(random(0, 500), 250, 0, 255, 0, random(2, 5));
+      ballArray.push(temp);
    }
 }
  
@@ -14,27 +14,29 @@ function draw() {
    background(0);
  
    for (let i = 0; i < ballArray.length; i++) {
-       fill(ballArray[i].redValue, ballArray[i].greenValue, ballArray[i].blueValue);
-       ellipse(ballArray[i].xPos, ballArray[i].yPos, 50, 50);
+      fill(ballArray[i].redValue, ballArray[i].greenValue, ballArray[i].blueValue);
+      ellipse(ballArray[i].xPos, ballArray[i].yPos, 50, 50);
  
-       ballArray[i].yPos += ballArray[i].speedValue;
+      ballArray[i].yPos += ballArray[i].speedValue;
  
-       if (ballArray[i].yPos > 525) {
-           ballArray[i].yPos = -25;
-       }
+      if (ballArray[i].yPos > 525) {
+         ballArray[i].yPos = -25;
+      }
    }
 }
  
 class Ball {
    constructor(x, y, r, g, b, speed ) {
-       this.xPos = x;
-       this.yPos = y;
-       this.redValue = r;
-       this.greenValue = g;
-       this.blueValue = b;
-       this.speedValue = speed;
+      this.xPos = x;
+      this.yPos = y;
+      this.redValue = r;
+      this.greenValue = g;
+      this.blueValue = b;
+      this.speedValue = speed;
    }
 }
+
+
 
 
 
